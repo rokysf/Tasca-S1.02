b302391b8ef8d9fd9e04098e6c8c59a1f75965a6
rokysf <135695631+rokysf@users.noreply.github.com>
2023-06-13 12:53:52 +0200

    Add files via upload

---
+++ src/n1exercici1/Producte.java
@@ -0,0 +1,17 @@
+package n1exercici1;
+
+public class Producte {
+
+	private String nom;
+	private int preu;
+
+	public Producte(String nom, int preu) {
+		this.nom = nom;
+		this.preu = preu;
+	}
+
+	public int getPreu() {
+		return preu;
+	}
+
+}
